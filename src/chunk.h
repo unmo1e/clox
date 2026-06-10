f47263f7292fe0a5a25630944a1edc05f0c1b75c
unknown <lomnainawan@gmail.com>
2026-06-10 11:02:20 +0530

    Boolean, Nil and Comparision Operators

--- src/chunk.h
+++ src/chunk.h
@@ -7,11 +7,21 @@
 
 typedef enum {
   OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  
   OP_ADD,
   OP_SUBTRACT,
   OP_MULTIPLY,
   OP_DIVIDE,
   OP_NEGATE,
+
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_NOT,
+
   OP_RETURN
 } OpCode;
 
--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "common.h"
 #include "vm.h"
@@ -7,14 +8,40 @@
 
 VM vm;
 
+Value pop();
+void push(Value value);
+static Value peek(int distance);
+static void resetStack();
+
+static void runtimeError(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int line = vm.chunk->lines[instruction];
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack();
+}
+
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)                           \
-  do {                                          \
-    double b = pop();                           \
-    double a = pop();                           \
-    push(a op b);                               \
+#define BINARY_OP(valueType, op)                                        \
+  do {                                                                  \
+    if(!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) {                    \
+      runtimeError("Operands must be numbers for binary operation.");   \
+      return INTERPRET_RUNTIME_ERROR;                                   \
+    }                                                                   \
+    double b = AS_NUMBER(pop());                                        \
+    double a = AS_NUMBER(pop());                                        \
+    push(valueType(a op b));                                            \
   } while (false)
 
 
@@ -37,6 +64,9 @@ static InterpretResult run() {
       push(constant);
       break;
     }
+    case OP_NIL: push(NIL_VAL); break;
+    case OP_TRUE: push(BOOL_VAL(true)); break;
+    case OP_FALSE: push(BOOL_VAL(false)); break;
 
     case OP_RETURN: {
       printValue(pop());
@@ -44,15 +74,29 @@ static InterpretResult run() {
       return INTERPRET_OK;
     }
 
-    case OP_ADD:      { BINARY_OP(+); break; }
-    case OP_SUBTRACT: { BINARY_OP(-); break; }
-    case OP_MULTIPLY: { BINARY_OP(*); break; }
-    case OP_DIVIDE:   { BINARY_OP(/); break; }
-
+    case OP_ADD:      { BINARY_OP(NUMBER_VAL, +); break; }
+    case OP_SUBTRACT: { BINARY_OP(NUMBER_VAL, -); break; }
+    case OP_MULTIPLY: { BINARY_OP(NUMBER_VAL, *); break; }
+    case OP_DIVIDE:   { BINARY_OP(NUMBER_VAL, /); break; }
     case OP_NEGATE: {
-      push(-pop());
+      if(!IS_NUMBER(peek(0))) {
+        runtimeError("Operand must be a number for negation.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
+
+      push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
     }
+
+    case OP_EQUAL: {
+      Value b = pop();
+      Value a = pop();
+      push(BOOL_VAL(valuesEqual(a,b)));
+      break;
+    }
+    case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
+    case OP_LESS:    BINARY_OP(BOOL_VAL, <); break;
+    case OP_NOT: { push(BOOL_VAL(isFalsey(pop()))); break; }
     }
   }
 
@@ -94,6 +138,10 @@ InterpretResult interpret(const char *source) {
 void push(Value value) {
   *vm.stackTop = value;
   vm.stackTop++;
+}
+
+static Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
 }
 
 Value pop() {
